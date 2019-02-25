d5ea7e18ead6b80a38158441bbaa7b14ab6a7bc0
John Carlo Salter <juuncaerlum@gmail.com>
2019-02-24 23:08:01 -0500

    add: Adding some other unit tests.

--- cc/tests/cc_test.go
+++ cc/tests/cc_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+
+
 func TestHasValidTag_ValidTagPasses(t *testing.T) {
 	//testMessage := "fix: this is a valid message."
 	cm := cc.CommitMessage {
@@ -13,8 +15,50 @@ func TestHasValidTag_ValidTagPasses(t *testing.T) {
 		Footer: []string{ "This is a footer"},
 		Description: "This is a description.",
 	}
+
 	_, err := cc.HasValidTag(cm)
 	if(err != nil) {
+		t.Fail()
+	}
+}
+
+func TestHasValidTag_InvalidTagIsRejected(t *testing.T) {
+	cm := cc.CommitMessage {
+		Type:  "badtag",
+		Body: []string{ "This is a body" },
+		Footer: []string{ "This is a footer"},
+		Description: "This is a description.",
+	}
+
+	_, err := cc.HasValidTag(cm)
+	if(err == nil) {
+		t.Fail()
+	}
+
+}
+
+func TestValidTag_EmptyTagIsRejected(t *testing.T) {
+	cm := cc.CommitMessage {
+		Type:  "",
+		Body: []string{ "This is a body" },
+		Footer: []string{ "This is a footer"},
+		Description: "This is a description.",
+	}
+	_, err := cc.HasValidTag(cm)
+	if(err == nil) {
+		t.Fail()
+	}
+}
+
+func TestValidTag_NilTagRejected(t *testing.T) {
+	cm := cc.CommitMessage {
+		Type:  nil,
+		Body: []string{ "This is a body" },
+		Footer: []string{ "This is a footer"},
+		Description: "This is a description.",
+	}
+	_, err := cc.HasValidTag(cm)
+	if(err == nil) {
 		t.Fail()
 	}
 }
